6a153a2520e39ceda5240f865cbd773ca2bd9227
javier zandate <mzandate@uncc.edu>
2017-09-30 21:47:59 +0000

    ready for heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,6 @@
 class HangpersonGame
 
+    attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -9,12 +10,58 @@ class HangpersonGame
   # end
   
   def initialize(word)
+    #puts "Created a new game"
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    
+    if letter == nil or letter == "" or !letter.match(/[A-Za-z]/)
+      throw "Error"
+    end
+    
+    if  @guesses.downcase.include? letter.downcase or  @wrong_guesses.downcase.include? letter.downcase
+      return false
+    end
+    
+    if @word.downcase.include? letter.downcase
+        @guesses += letter
+    else
+        @wrong_guesses += letter
+    end
+  end
+  
+#  def word_with_guesses
+#    self.word_with_guesses(@word, @guesses)
+#  end
+  
+  def word_with_guesses(word=@word, guesses=@guesses)
+    word.gsub(/[^ #{guesses}]/, '-')
+#    wwg = @word;
+#    
+#    @word.downcase.split("").each do |i|
+#      if not @guesses.downcase.include? i
+#        wwg.sub!(i, "-")
+#      end
+#    end
+#    
+#    return wwg
+  end
+  
+  def check_win_or_lose
+    
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    
+    if not word_with_guesses.include? "-"
+      return :win
+    end 
+    
+    return :play
+  end
   def self.get_random_word
     require 'uri'
     require 'net/http'
